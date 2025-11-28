f59a0a450756248bc4b72095346252554b38b8c5
MD SIFAT <kakasihatak8@gmail.com>
2025-11-28 13:41:30 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "fork",
     aliases: ["repo", "source"],
     version: "1.0",
-    author: "NeoKEX",
+    author: "SiFu",
     countDown: 3,
     role: 0,
     longDescription: "Returns the link to the official, updated fork of the bot's repository.",
@@ -12,8 +12,8 @@ module.exports = {
   },
 
   onStart: async function({ message }) {
-    const text = "✓ | Here is the updated fork:\n\nhttps://github.com/NeoKEX/Goatbot-updated.git\n\n" +
-                 "Changes:\n1. No Google Credentials needed\n2. Enhanced overall performance\n3. Now using fca-neokex (v1.0.1)\n\n" +
+    const text = "☠️ | Here is the updated fork:\n\nhttps:🍴🍴🍴\n\n" +
+                 "\nEnhanced overall performance\n\n" +
                  "Keep supporting^_^";
     
     message.reply(text);
